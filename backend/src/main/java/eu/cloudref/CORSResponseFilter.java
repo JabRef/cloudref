5a1c5d7e08971ec4a4abd8a509c13e07a2a4b4af
Oliver Kopp <kopp.dev@gmail.com>
2018-01-22 16:54:40 +0100

    Adapt Access-Control-Allow-Origin header

--- backend/src/main/java/eu/cloudref/CORSResponseFilter.java
+++ backend/src/main/java/eu/cloudref/CORSResponseFilter.java
@@ -10,7 +10,9 @@ public class CORSResponseFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext creq, ContainerResponseContext cres) {
-        cres.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200"); // allow CORS requests from frontend
+        // development: localhost:4200 - however, we might be hosted anywhere - quick hack
+        cres.getHeaders().add("Access-Control-Allow-Origin", "*");
+
         cres.getHeaders().add("Access-Control-Allow-Headers", "Authorization, Accept, Origin, X-Requested-With, Content-Type, Last-Modified");
         cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
         cres.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, HEAD, PUT, DELETE, OPTIONS");
